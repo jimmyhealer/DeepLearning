74a09de7cc6e981666fe89cf5eae3f7c0c1fb9f0
jimmyhealer <yahing6066@gmail.com>
2025-02-08 21:18:58 +0800

    feat: Update image_merge

--- utils/image_merge.py
+++ utils/image_merge.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
 # 讀取圖片
-image1 = Image.open('/home/jimmyhealer/Documents/DeepLearning/A6/assets/wgan_result.png')
-image2 = Image.open('A6/assets/wgan_gp_result.png')
+image1 = Image.open('A8/assets/result1.png')
+image2 = Image.open('A8/assets/result2.png')
 
 # 確保兩張圖片的高度相同，如果需要可以調整大小
 image1 = image1.resize((image1.width, image2.height))
@@ -16,4 +16,4 @@ merged_image.paste(image1, (0, 0))
 merged_image.paste(image2, (image1.width, 0))
 
 # 保存或顯示結果
-merged_image.save('./A6/assets/wgan_and_wgan_gp_result.png')
+merged_image.save('./A8/assets/result.png')
